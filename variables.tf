09565d454e1de792df0bb70e7ebc14749884020e
Mikalai Yatsyna <nikolai.yatsyna@gmail.com>
2023-03-07 23:16:01 +0400

    initial secrets
    Signed-off-by: Mikalai Yatsyna <nikolai.yatsyna@gmail.com>

--- variables.tf
+++ variables.tf
@@ -11,5 +11,6 @@ variable "vault_role_name" {
 variable "backends" {
   type        = list(string)
   description = "List of backends to enable"
-  default     = ["kv-v2", "database", "consul"]
+
+  default = ["kv-v2", "database", "consul"]
 }
